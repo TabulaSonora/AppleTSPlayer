8d3e86114442d9e20ea5298458648d21d0fcf6e5
Kevin López Brante <kevin@kddlb.cl>
2026-08-06 23:38:00 -0400

    Initial Commit

---
+++ Tabula Sonora Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tabula Sonora Player
+//
+//  Created by Kevin López Brante on 06-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tabula Sonora Player/Tabula_Sonora_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabula_Sonora_PlayerApp.swift
+//  Tabula Sonora Player
+//
+//  Created by Kevin López Brante on 06-08-26.
+//
+
+import SwiftUI
+
+@main
+struct Tabula_Sonora_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
